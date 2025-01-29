c05438178c31ff0b72b2167c129624e3dd02227b
nafis585 <princetoure@gmail.com>
2025-01-29 17:56:01 +0000

    Added project file--

---
+++ src/begin_react/04. Calculator/App.js
@@ -0,0 +1,11 @@
+import Calculatrice from "./calculatrice";
+
+function App() {
+  return (
+    <div>
+      <Calculatrice />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/begin_react/07. Form validation/App.js
@@ -0,0 +1,11 @@
+import Validation from "./begin_react/07. Form validation/Validation";
+
+function App() {
+  return (
+    <div>
+      <Validation />
+    </div>
+  );
+}
+
+export default App;
